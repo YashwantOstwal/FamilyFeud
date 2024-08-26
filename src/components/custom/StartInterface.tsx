a1f114514ab1a1151dc2c43c58b83fab287c1d37
Yashwant <yashwant08012005@gmail.com>
2024-08-26 08:10:00 +0530

    changed bg opacity

--- src/components/custom/StartInterface.tsx
+++ src/components/custom/StartInterface.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function StartInterface({ setStarted }: Props) {
   return (
     <>
-      <div className='absolute inset-0 bg-black/95' />
+      <div className='absolute inset-0 bg-black/80' />
       <div className='absolute top-[10%] inset-x-0 inset-y-[20%] flex flex-col justify-around items-center'>
         <div className='w-1/4 flex justify-between'>
           <motion.div initial={{ x: '-25px' }} animate={{ x: '0px' }} transition={{ delay: 0.2, type: 'tween' }}>
