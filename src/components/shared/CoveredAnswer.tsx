51845dfa0f678686714dca1e623c5c5048756ec6
Yashwant <yashwant08012005@gmail.com>
2024-08-26 14:19:30 +0530

    added Timer

--- src/components/shared/CoveredAnswer.tsx
+++ src/components/shared/CoveredAnswer.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction } from 'react';
+import React from 'react';
 import Answer from '@components/base/Answer';
 import Cover from '@components/base/Cover';
 interface CoveredAnswerProps {
--- src/containers/GameContainer/index.tsx
+++ src/containers/GameContainer/index.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import HeaderView from '@views/HeaderView';
 import AnswersView from '@views/AnswersView';
 import InputView from '@views/InputView';
-import RenderGameOver from '@components/base/RenderGameOver';
+import RenderGameOver from '@components/custom/RenderGameOver';
 import StartInterface from '@components/custom/StartInterface';
 interface answerElementType {
   answer: string;
@@ -13,6 +13,7 @@ interface dataType {
   question: string;
   answers: answerElementType[];
 }
+let interval: NodeJS.Timer;
 const GameContainer: React.FC = () => {
   const data: dataType = {
     question: 'Social _______',
@@ -43,42 +44,49 @@ const GameContainer: React.FC = () => {
       },
     ],
   };
-  const [time, setTime] = useState<number>(40);
+  const [time, setTime] = useState<number>(5);
   const [score, setScore] = useState<number>(0);
   const [guesses, setGuesses] = useState<string[]>([]);
   const [started, setStarted] = useState<boolean>(false);
+  //Calculating score whenever the "guesses" changes by checking the new entry in the array as one of the answers(no duplication of words in guesses array)
+
   useEffect(() => {
     const element = data.answers.find((element) => {
       return element.answer == guesses[guesses.length - 1];
     });
     element && setScore(element.points + score);
   }, [guesses]);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setTime(time - 1);
-  //   }, 1000);
-  // }, [time]);
-  //bg-[#172554]
+  useEffect(() => {
+    setTimeout(() => {
+      if (started) {
+        interval = setInterval(() => {
+          setTime((prevState) => --prevState);
+        }, 1000);
+      }
+    }, 0);
+  }, [started]);
+  if (time == 0) {
+    clearInterval(interval);
+  }
   return (
-    <div className=' bg-gradient-to-b from-[#000110] from-30% to-[#00099b] h-screen'>
-      <motion.div
-        initial={{ scale: 0.8 }}
-        animate={{ scale: started ? 1 : 0.8 }}
-        transition={{ duration: 0.5, type: 'tween' }}
-        className=' h-screen flex flex-col justify-between items-center overflow-hidden '
-      >
-        {true ? (
+    <>
+      <div className=' bg-gradient-to-b from-[#000110] from-30% to-[#00099b] h-screen'>
+        <motion.div
+          initial={{ scale: 0.8 }}
+          animate={{ scale: started ? 1 : 0.8 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className=' h-screen flex flex-col justify-between items-center overflow-hidden '
+        >
           <>
             <HeaderView started={started} question={data.question} seconds={time} score={score} />
             <AnswersView guesses={guesses} setScore={setScore} answers={data.answers} score={score} />
             <InputView started={started} setGuesses={setGuesses} guesses={guesses} />
           </>
-        ) : (
-          <RenderGameOver />
-        )}
-      </motion.div>
+        </motion.div>
+      </div>
       {!started && <StartInterface setStarted={setStarted} />}
-    </div>
+      {time <= 0 && <RenderGameOver />}
+    </>
   );
 };
 export default GameContainer;
