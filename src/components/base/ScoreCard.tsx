0a08ec7fcf79e252ad4240323fb08288f280baa5
Yashwant <yashwant08012005@gmail.com>
2024-08-28 23:27:24 +0530

    modified Css Colors

--- src/components/base/ScoreCard.tsx
+++ src/components/base/ScoreCard.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-const ScoreCard = ({ score, started }: { score?: number; started: boolean }) => {
+const ScoreCard = ({ score, started }: { score: number; started: boolean }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: started ? 1 : 0 }}
       transition={{ delay: 0.5 }}
-      className={`text-center text-[1.5vw] text-white ${score && 'text-red-700'}`}
+      className={`text-center text-[1.5vw] ${score < 0 ? 'text-red-700' : 'text-white'}`}
     >
       {score}
     </motion.div>
